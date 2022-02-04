ffe864103ce11f05a2b8b292af8ff5ed0253df37
masyaf13 <serkan.ozdemir.1990@gmail.com>
2022-02-04 20:40:02 +0300

    Add project files.

---
+++ BookShop/Book.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BookShop
+{
+    public class Book : BaseClass
+    {
+        public static List<Book> Books = new List<Book>();
+        public string Name { get; set; } // kitabın adı
+        public BookTypeEnums BookType { get; set; } // kitap türü
+        public double CostPrice { get; set; } // maliyet fiyati
+        public int TaxPercantage { get; set; } // toplam vergiyi belirtir
+        public int ProfitMargin { get; set; } // kazanç yüzde ör. 15 => %18
+        public double Price { get; set; } // ürün fiyatı
+        public int QTY { get; set; } // quantity qty -> stokdaki adedi gösterir
+
+
+        // constructor
+        public Book(string _name, double _costPrice, BookTypeEnums _bookTypeEnums, int _taxPercantage = 1, int _profitMargin = 10, int _qty = 1)
+        {
+            Name = _name;
+            CostPrice = _costPrice;
+            BookType = _bookTypeEnums;
+            TaxPercantage = _taxPercantage;
+            ProfitMargin = _profitMargin;
+            QTY = _qty;
+
+            Price = calculatePrice(_costPrice, _taxPercantage, _profitMargin);
+        }
+
+        public double calculatePrice(double costPrice, int tax, int profitMargin)
+        {
+
+            //methodda verilen isimler olmalı
+
+            double taxPrice = (costPrice * tax) / 100;
+            double profitPrice = (costPrice * profitMargin) / 100;
+            double price = costPrice + taxPrice + profitPrice;
+            return price;
+        }
+
+        public static void addBook(Book book)
+        {
+            try
+            {
+                // kitap oluştruldu
+                Books.Add(book);
+
+                // ürün maliyeti hesaplanan metot sayesinde metot hesaplandı
+                double amount = CaseTransaction.calculateAmount(book.CostPrice, book.QTY);
+
+                //kasa hareketleri nesnesi oluşturuldu
+                CaseTransaction caseTransaction = new CaseTransaction(amount, TransactionTypeEnums.EXPENSE);
+
+                //kasa hareketini kaydetmek için CaseTransaction sınıfındaki save metodu  çağrıldı
+                CaseTransaction.saveCaseTransaction(caseTransaction);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hata oluştu: " + ex.Message);
+            }
+
+        }
+
+        public static void removeBook(int Id)
+        {
+
+            foreach (Book book in Books)
+            {
+                if (book.Id == Id)
+                {
+                    Books.Remove(book);
+                    Console.WriteLine("Kitabınız başarıyla silindi");
+                    break;
+                }
+            }
+        }
+
+ 
+
+
+        //satılacak kitabın id sini ve kaç kitap olduğu bilgisini alın
+        public static void sellBook(int bookId, int bookQty)
+        {
+            foreach (Book kitap in Books)
+            {
+                if (kitap.Id == bookId)
+                {
+                    // satılan kitap miktarı kitap listesindeki kitap bilgisinden çkarıldı
+                    // satılan kitap miktarı toplam miktardan büyükse hata verir
+                    kitap.QTY = kitap.QTY - bookQty;
+
+                    //satış tutarı hesaplandı 
+                    double satisTutar = CaseTransaction.calculateAmount(kitap.Price, bookQty);
+
+                    // kasa hareketinden yeni bir object 
+                    CaseTransaction kasaHaraketi = new CaseTransaction(satisTutar, TransactionTypeEnums.INCOMING);
+
+                    //
+                    CaseTransaction.saveCaseTransaction(kasaHaraketi);
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return String.Format(" Id: {0} " + "Name : {1} " + "Type:{2} " + "Cost Price: {3} " + "Price {4} " + "Quantity:{5} ", Id, Name, BookType, CostPrice, Price,QTY);
+        }
+
+    }
+}
---
+++ BookShop/Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+
+namespace BookShop
+{
+    public class Program
+    {
+
+        /// <summary>
+        /// Kitap Mağazası Uygulaması
+        ///
+        /// Kitap, Kasa
+        ///
+        /// 1 - kitap kayıt edebilmeyi
+        ///     -- kayıt esnasında kitap adi, adedi, maliyet fiyati, vergisi, kazanç miktari vs.
+        ///     -- ürün fiyati maliyet fiyati, vergi ve kazanç miktarina bağlı olarak hesaplanır
+        /// 2 - kitap silebilme
+        ///     -- kita silme fonksiyonu seçilirse girilen adet kadar kitap silinecektir
+        /// 3 - kitap güncelleme
+        /// 4 - kitap satış
+        ///     -- satılan kitap fiyatı kasaya gelir olarak giriş yapılır
+        ///     -- satılan kitap kitap listemden eksiltilir
+        /// 5 - kitap listesi
+        /// 6 - kitap listesinden arama kabiliyeti
+        ///
+        ///  Kitap -> id, adi, tür'ü (enum kullanacağız), maliyet fiyati, toplam vergi, stok adedi, kayit tarihi, güncelleme tarihi
+        ///  Kasa işlemi -> id, tür (gelir , gider (enum kullanacağım)), tutar, kayit tarihi
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+
+            int secim = 0;
+            while (secim != 8)
+            {
+                Console.WriteLine("1 - Kitap Ekle");
+                Console.WriteLine("2 - Kitap Silme");
+                Console.WriteLine("3 - Kitap Güncelleme");
+                Console.WriteLine("4 - Kitap Satış");
+                Console.WriteLine("5 - Kitap Listeleme");
+                Console.WriteLine("6 - Kitap ara");
+                Console.WriteLine("7 - Kasa Hareketleri");
+                Console.WriteLine("8 - Çıkış");
+
+                secim = Convert.ToInt32(Console.ReadLine());
+
+
+
+                switch (secim)
+                {
+                    case 1:
+                        kitapEkleCase();            
+                        break;
+                    case 2:
+                        kitaplariListele();
+                        Console.Write("Silmek istediğiniz kitap id si: ");
+                        int bookId = Convert.ToInt32(Console.ReadLine());
+                        Book.removeBook(bookId);
+                        break;
+                    case 4:
+                        kitaplariListele();
+                        Console.Write("Satılan kitap id'si : ");
+                        int satilanKitapId = Convert.ToInt32(Console.ReadLine());
+
+                        Console.Write("Satılan Kitap Adedi:");
+                        int satilanKitapAdeti = Convert.ToInt32(Console.ReadLine());
+
+                        Book.sellBook(satilanKitapId,satilanKitapAdeti);
+                        kitaplariListele();
+                        break;
+                    case 5:
+                        kitaplariListele();                      
+                        break;
+                    case 6:
+                                               
+                        break;
+                    case 7:
+                        CaseTransaction.kasaHareketleriniListele();
+
+                        break;
+                    default:
+                        break;
+                        
+                        }
+
+                }
+
+            /*
+            int sayac = 0;
+
+            Random rastgele = new Random();
+
+             while (sayac < 50)
+            {
+                // yeni bir kitap nesnesi oluşturuldu
+                Book book = new Book("Kitap " + (sayac +1), rastgele.Next(35,51), (BookTypeEnums)rastgele.Next(0,5), rastgele.Next(1,21), rastgele.Next(10,31) , rastgele.Next(1,16) );
+                //oluşturulan kitap listeye eklendi
+                Book.addBook(book);
+                sayac++;
+            }
+
+
+            */
+
+            Console.WriteLine("KASA HAREKETLERİ");
+            foreach (CaseTransaction caseTransaction in CaseTransaction.CaseTransactions)
+            {
+                Console.WriteLine("------------------------------");
+                Console.WriteLine(caseTransaction.ToString());
+
+
+            }
+            
+        }
+        public static void kitapEkleCase()
+        {
+            Console.Write("Kitap Adı:");
+            string bookName = Console.ReadLine();
+
+            Console.Write("Maliyet:");
+            double costPrice = Convert.ToDouble(Console.ReadLine());
+
+          
+            Console.Write("Türü(0-4):");
+            BookTypeEnums bookType = (BookTypeEnums)Convert.ToInt32(Console.ReadLine());
+            if ( bookType< 0 && 4< (int)bookType)
+            {
+                Console.WriteLine("lütfen 0-4 arasında değer girin");
+            }
+        
+            Console.Write("Vergi Oranı:");
+            int taxPercantage = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Kazanç Oranı:");
+            int profitMargin = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Adet:");
+            int qty = Convert.ToInt32(Console.ReadLine());
+
+            Book newBook = new Book(bookName, costPrice, bookType, taxPercantage, profitMargin, qty);
+            Book.addBook(newBook);
+        }
+
+        public static void kitaplariListele()
+        {
+
+            Console.WriteLine("KİTAP LİSTESİ");
+            foreach (Book item in Book.Books)
+            {
+                Console.WriteLine("------------------------------");
+                Console.WriteLine(item.ToString());
+
+
+            }
+        }
+       
+      
+    }
+}
